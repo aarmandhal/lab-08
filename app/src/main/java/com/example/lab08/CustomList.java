27ca4d70647781467044cb29a9c42ffc5ecda55d
aarmandhal <adhal@ualberta.ca>
2025-10-31 12:58:37 -0600

    Test Delete Failure

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -18,7 +18,6 @@ public class CustomList {
         return cities.contains(city);
     }
 
-    
 
     // Will be implemented later using TDD (leave blank for now)
     // public boolean hasCity(City city) { ... }
